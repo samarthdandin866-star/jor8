02ea4a8846fdfca9d9a260f849ce7832a671dcb8
Samarth Dandin <samarthdandin866@gmail.com>
2025-12-03 19:22:41 +0530

    save

---
+++ employee.py
@@ -0,0 +1,20 @@
+# employee.py
+
+def employee_details(name, emp_id, department, salary):
+    result = (
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Department: {department}\n"
+        f"Salary: {salary}"
+    )
+    return result
+
+
+if __name__ == "_main_":
+    # Sample input
+    name = "Alice"
+    emp_id = "E0110"
+    department = "IT"
+    salary = 55000
+
+    print(employee_details(name, emp_id, department, salary))
